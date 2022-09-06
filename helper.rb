e820eb3ad0a56120a38b51383534bd2d6d382fe7
Paul Onchera <hesbonpoisson@gmail.com>
2022-09-06 13:16:14 +0300

    Add a helper file with options.

---
+++ helper.rb
@@ -0,0 +1,14 @@
+Module Options
+   def display_list
+    puts "
+    Please choose an option by entering a number:
+    1- List all books.
+    2- List all people.
+    3- Create a person (teacher or student, not a plain Person).
+    4- Create a book.
+    5- Create a rental.
+    6- List all rentals for a given person id.
+    7- Exit"
+  end
+
+end
