1cd92ffe69d5d7051c3e9646ee87a0dc8effa7e0
Jahongir Yusupov <91022355+jahongiry@users.noreply.github.com>
2022-09-06 15:47:28 +0500

    Fix Linter errors

--- helper.rb
+++ helper.rb
@@ -1,13 +1,13 @@
 module Options
-   def display_list
-    puts "
-    Please choose an option by entering a number:
-    1- List all books.
-    2- List all people.
-    3- Create a person (teacher or student, not a plain Person).
-    4- Create a book.
-    5- Create a rental.
-    6- List all rentals for a given person id.
-    7- Exit"
+  def display_list
+      puts "
+        Please choose an option by entering a number:
+        1- List all books.
+        2- List all people.
+        3- Create a person (teacher or student, not a plain Person).
+        4- Create a book.
+        5- Create a rental.
+        6- List all rentals for a given person id.
+        7- Exit"
   end
 end
